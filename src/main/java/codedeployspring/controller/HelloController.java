1346071148dd5ceadc713954662cc526a4c4d19d
raviMukti <shinjiravi@gmail.com>
2021-08-23 18:01:33 +0700

    initialize

---
+++ src/main/java/codedeployspring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package codedeployspring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
+public class HelloController {
+
+    @GetMapping(value = "/hello-world")
+    public ResponseEntity<Object> helloWorld(){
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+
+}
